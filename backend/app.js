4b2f232bde77c7e1ceaa30e7c0747a3b5bf3d1bc
MozartofCode <bertan.berker@gmail.com>
2024-12-30 13:19:49 -0500

    Creating a node.js backend

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+const axios = require('axios');
+
+app.use(express.json());
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
+
+app.post('/register', async (req, res) => {
+
+});
+
---
+++ backend/index.js
@@ -0,0 +1,16 @@
+// const express = require('express');
+// const app = express();
+// const port = 3000;
+
+// // Middleware
+// app.use(express.json());
+
+// // Routes
+// app.get('/', (req, res) => {
+//   res.send('Welcome to my Node.js Backend!');
+// });
+
+// // Start Server
+// app.listen(port, () => {
+//   console.log(`Server running at http://localhost:${port}`);
+// });
